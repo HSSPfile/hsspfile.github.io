04a9b256cee103886edb49bc17ed0454c324873d
LeoX DEV <84378319+Le0X8@users.noreply.github.com>
2023-06-12 10:06:20 +0200

    -

--- create.js
+++ create.js
@@ -11,11 +11,15 @@ const files = [];
     const fileReadEventHandler = (ev) => new Promise((resolve, reject) => {
         const file = ev.target.files[0];
         const reader = new FileReader();
-        reader.onload = () => resolve(reader.result);
+        reader.onload = () => resolve({name: file.name, buffer: reader.result, changed: file.lastModified});
         reader.readAsArrayBuffer(file);
     });
     
     upload.onchange = async (ev) => {
         const index = files.push(await fileReadEventHandler(ev));
+        editor.addFile(files[index].name, files[index].buffer, {
+            changed: new Date(files[index].name)
+        });
+        console.log(editor.files[files[index].name]);
     };
 })();
